b932d7585b78c1496dbec44c625c44b0d4d0dfa2
johnson111788 <50241800+johnson111788@users.noreply.github.com>
2020-06-21 00:20:55 +0800

    周昱程：游戏平衡控制（移动速度、商店购买次数、子弹数量、磁铁范围、敌人血量调整、金币获取）

---
+++ Pacman/Assets/Scripts/model/Pacdot.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class Pacdot : MonoBehaviour
+{
+    public bool isSuperPacdot = false;
+    public bool isMidPacdot = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Pacman")
+        {
+            if (isSuperPacdot)
+            {
+                GameManager.Instance.OnEatPacdot(gameObject);
+                GameManager.Instance.OnEatSuperPacdot();
+                Destroy(gameObject);
+            }
+
+            else if (isMidPacdot)
+            {
+                GameManager.Instance.OnEatPacdot(gameObject);
+                GameManager.Instance.OnEatMidPacdot();
+                Destroy(gameObject);
+            }
+
+            else
+            {
+                GameManager.Instance.OnEatPacdot(gameObject);
+                Destroy(gameObject);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "Pacman")
+        {
+            if (isSuperPacdot)
+            {
+                GameManager.Instance.OnEatPacdot(gameObject);
+                GameManager.Instance.OnEatSuperPacdot();
+                Destroy(gameObject);
+            }
+
+            else if (isMidPacdot)
+            {
+                GameManager.Instance.OnEatPacdot(gameObject);
+                GameManager.Instance.OnEatMidPacdot();
+                Destroy(gameObject);
+            }
+
+            else
+            {
+                GameManager.Instance.OnEatPacdot(gameObject);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Pacman/Assets/Scripts/model/Weapon.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public float BulletSpeed = 20f;
+    public static bool IsShooting;
+    //计时器控制子弹发射cd
+    private float timeVal;
+
+    public int bulletCount = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeVal >= 0.2f)
+        {
+            Attack();
+        }
+        else
+        {
+            timeVal += Time.deltaTime;
+        }
+    }
+
+    private void Attack()
+    {
+        if (GameManager.Instance.bulletCount > 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                // Vector3 m_mousePosition = Input.mousePosition;
+                // m_mousePosition = Camera.main.ScreenToWorldPoint(m_mousePosition);
+                // m_mousePosition.z = 0;
+                // float m_fireAngle = Vector2.Angle(m_mousePosition - this.transform.position, Vector2.up)-90 ;
+                //if (m_mousePosition.x > this.transform.position.x)
+                //{
+                //   m_fireAngle = -m_fireAngle;
+                // }
+                GameManager.Instance.AutoAdd();
+                timeVal = 0;
+
+                if (IsShooting == true)
+                {
+                    GameObject m_bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity) as GameObject;
+
+
+                    if (PacmanMove.faceUp)
+                    {
+                        m_bullet.GetComponent<Rigidbody2D>().velocity = ((Vector2.up).normalized * BulletSpeed);
+                    }
+                    else if (PacmanMove.faceDown)
+                    {
+                        m_bullet.GetComponent<Rigidbody2D>().velocity = ((Vector2.down).normalized * BulletSpeed);
+                    }
+                    else if (PacmanMove.faceLeft)
+                    {
+                        m_bullet.GetComponent<Rigidbody2D>().velocity = ((Vector2.left).normalized * BulletSpeed);
+                    }
+                    else if (PacmanMove.faceRight)
+                    {
+                        m_bullet.GetComponent<Rigidbody2D>().velocity = ((Vector2.right).normalized * BulletSpeed);
+                    }
+                    //   m_bullet.transform.eulerAngles = new Vector3(0, 0, m_fireAngle);
+                }
+            }
+        }
+    }
+
+    void Shoot()
+    {
+
+    }
+}
